31def55552face3573528c79f4c26e143dfc9a6a
waynechoidev <wonjun92@gmail.com>
2024-04-06 15:44:26 +1300

    01_triangle

---
+++ src/01_triangle/index.ts
@@ -0,0 +1,65 @@
+import main_vert from "/shader/main.vert.wgsl";
+import main_frag from "/shader/main.frag.wgsl";
+
+const WIDTH = document.documentElement.clientWidth;
+const HEIGHT = document.documentElement.clientHeight;
+
+const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+const adapter: GPUAdapter = (await navigator.gpu.requestAdapter())!;
+const device: GPUDevice = await adapter.requestDevice();
+const context: GPUCanvasContext = canvas.getContext("webgpu")!;
+const presentationFormat: GPUTextureFormat =
+  navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+  device,
+  format: presentationFormat,
+  alphaMode: "premultiplied",
+});
+
+const pipeline: GPURenderPipeline = device.createRenderPipeline({
+  layout: "auto",
+  vertex: {
+    module: device.createShaderModule({
+      code: main_vert,
+    }),
+  },
+  fragment: {
+    module: device.createShaderModule({
+      code: main_frag,
+    }),
+    targets: [
+      {
+        format: presentationFormat,
+      },
+    ],
+  },
+  primitive: {
+    topology: "triangle-list",
+  },
+});
+
+function render() {
+  const renderPassDescriptor: GPURenderPassDescriptor = {
+    colorAttachments: [
+      {
+        view: context.getCurrentTexture().createView(),
+        clearValue: [0, 0, 0, 1],
+        loadOp: "clear",
+        storeOp: "store",
+      },
+    ],
+  };
+
+  const encoder = device.createCommandEncoder({ label: "our encoder" });
+  const pass = encoder.beginRenderPass(renderPassDescriptor);
+  pass.setPipeline(pipeline);
+  pass.draw(3); // call our vertex shader 3 times
+  pass.end();
+
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);
+}
+
+requestAnimationFrame(render);
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
+
+const config = defineConfig({
+  server: {
+    port: 8888,
+  },
+  plugins: [glsl()],
+});
+
+export default config;
