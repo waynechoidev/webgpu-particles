e98bf9b910add3c7117082ec722c2142fff6289f
waynechoidev <wonjun92@gmail.com>
2024-04-08 10:29:07 +1200

    04_checker-board

--- src/04_checker-board/index.ts
+++ src/04_checker-board/index.ts
@@ -67,7 +67,23 @@ const main = async () => {
     device.queue.submit([commandBuffer]);
   }
 
-  requestAnimationFrame(render);
+  const observer = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      const canvas = entry.target as HTMLCanvasElement;
+      const width = entry.contentBoxSize[0].inlineSize;
+      const height = entry.contentBoxSize[0].blockSize;
+      canvas.width = Math.max(
+        1,
+        Math.min(width, device.limits.maxTextureDimension2D)
+      );
+      canvas.height = Math.max(
+        1,
+        Math.min(height, device.limits.maxTextureDimension2D)
+      );
+      render();
+    }
+  });
+  observer.observe(canvas);
 };
 
 main();
--- src/04_checker-board/main.frag.wgsl
+++ src/04_checker-board/main.frag.wgsl
@@ -4,7 +4,7 @@
   let red = vec4f(1, 0, 0, 1);
   let cyan = vec4f(0, 1, 1, 1);
 
-  let grid = vec2u(fsInput.position.xy) / 4;
+  let grid = vec2u(fsInput.position.xy) / 8;
   let checker : bool = (grid.x + grid.y) % 2 == 1;
 
   return select(red, cyan, checker);
