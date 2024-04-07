0c6b2c03f2848f0d6b104a0688d468ac36dee57a
waynechoidev <wonjun92@gmail.com>
2024-04-07 23:27:30 +1200

    02-doubling

---
+++ src/02_doubling/index.ts
@@ -0,0 +1,80 @@
+import main_compute from "./main.compute.wgsl";
+
+const main = async () => {
+  const adapter = await navigator.gpu?.requestAdapter();
+  const device = await adapter?.requestDevice()!;
+  if (!device) {
+    alert("need a browser that supports WebGPU");
+    return;
+  }
+
+  const pipeline = device.createComputePipeline({
+    label: "doubling compute pipeline",
+    layout: "auto",
+    compute: {
+      module: device.createShaderModule({
+        label: "doubling compute shader",
+        code: main_compute,
+      }),
+    },
+  });
+
+  const input = new Float32Array([1, 3, 5]);
+
+  // create a buffer on the GPU to hold our computation
+  // input and output
+  const workBuffer = device.createBuffer({
+    label: "work buffer",
+    size: input.byteLength,
+    usage:
+      GPUBufferUsage.STORAGE |
+      GPUBufferUsage.COPY_SRC |
+      GPUBufferUsage.COPY_DST,
+  });
+  // Copy our input data to that buffer
+  device.queue.writeBuffer(workBuffer, 0, input);
+
+  // create a buffer on the GPU to get a copy of the results
+  const resultBuffer = device.createBuffer({
+    label: "result buffer",
+    size: input.byteLength,
+    usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+  });
+
+  // Setup a bindGroup to tell the shader which
+  // buffer to use for the computation
+  const bindGroup = device.createBindGroup({
+    label: "bindGroup for work buffer",
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [{ binding: 0, resource: { buffer: workBuffer } }],
+  });
+
+  // Encode commands to do the computation
+  const encoder = device.createCommandEncoder({
+    label: "doubling encoder",
+  });
+  const pass = encoder.beginComputePass({
+    label: "doubling compute pass",
+  });
+  pass.setPipeline(pipeline);
+  pass.setBindGroup(0, bindGroup);
+  pass.dispatchWorkgroups(input.length);
+  pass.end();
+
+  // Encode a command to copy the results to a mappable buffer.
+  encoder.copyBufferToBuffer(workBuffer, 0, resultBuffer, 0, resultBuffer.size);
+
+  // Finish encoding and submit the commands
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);
+
+  // Read the results
+  await resultBuffer.mapAsync(GPUMapMode.READ);
+  const result = new Float32Array(resultBuffer.getMappedRange().slice(0));
+  resultBuffer.unmap();
+
+  console.log("input", input);
+  console.log("result", result);
+};
+
+main();
